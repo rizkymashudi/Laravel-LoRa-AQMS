f81bef506ba3e4515313c9c52a0174b397040eb2
rizkymashudi <rizkymashudi7@gmail.com>
2020-07-21 16:00:55 +0700

    tambah waktu sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,6 +3,16 @@
     {{-- <div class="image">
       <img src="{{ url('AdminLTE/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
     </div> --}}
+    <a href="javascript:void(0)" class="sa-sidebar-shortcut-toggle" style="color:white;">
+      <p id="waktu" align="center"></p>
+      <script>
+          var myVar = setInterval(myTimer ,1000);
+          function myTimer() {
+            var d = new Date();
+            document.getElementById("waktu").innerHTML = d.toLocaleTimeString();
+          }
+      </script>
+  </a>
  </div>
 
   <!-- Sidebar Menu -->
