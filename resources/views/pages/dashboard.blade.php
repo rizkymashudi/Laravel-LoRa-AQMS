c52826bae03b07bb23c6752657e5b98178051933
rizkymashudi <rizkymashudi7@gmail.com>
2020-07-23 15:30:55 +0700

    push update

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -29,7 +29,7 @@
             <!-- BAR CHART -->
             <div class="card card-dark">
               <div class="card-header">
-                <h3 class="card-title">Grafik Index Stadar Pencemaran Udara (ISPU)</h3>
+                <h3 class="card-title">Grafik Index Standar Pencemaran Udara (ISPU)</h3>
                 <div class="card-tools">
                   <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
                 </div>
@@ -423,7 +423,8 @@
               // xAxes: [{
               //   barPercentage: 0.4
               // }]
-          }
+          },
+          
         }
 
         var myLineChart = new Chart(lineChartPM, {
